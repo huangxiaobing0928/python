dd1e1ceec18706e8567d898c754c4a10a46eca98
gaojunhao <197622534@qq.com>
2019-12-23 15:40:41 +0000

    add datatype knowledge the second time

---
+++ base/datatypes/unchanged/Number.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+a,b,c,d = 20,5.5,True,1+2j
+print(type(a),type(b),type(c),type(d))
+
+'''
+number type:
+int
+float
+bool
+complex
+'''
+
+a = 123
+print(isinstance(a,int))
+
+
+
