8b5fc161f04fd658060255f878984173fe50c4ec
gaojunhao <197622534@qq.com>
2019-12-24 16:07:56 +0000

    add notes the third time

---
+++ base/datatypes/changed/List.py
@@ -0,0 +1,20 @@
+list1 = ['abc',3.3,24,5+2j]
+list2 = ['I','LOVE','YOU']
+
+print(list1[0:5])
+print(list1[1:])
+print(list2[1])
+print(list2 * 2)
+print(list1 + list2)
+
+list1[0:1] =['def','999']
+list2[2] = 'YOU TOO'
+list1[2:] = []
+
+print(list1)
+print(list2)
+
+print(list2[0::2])
+
+
+
---
+++ base/datatypes/unchanged/Tuple.py
@@ -0,0 +1,20 @@
+'''
+tip1
+the differences between tuple and list
+one:the element of tuple cannot be changed , like string
+two:tuple use (), but list use []
+
+tip2
+string can be regarded as a special tuple
+
+tip3:special rule
+'''
+tuple1 = () #create an empty tuple
+print(tuple1)
+tuple2 = (12,) #When you  create a tuple including one element , you need add , after the element
+print(tuple2)
+
+'''
+tip4
+string , list and tuple all belong to sequence
+'''
