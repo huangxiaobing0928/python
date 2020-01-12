94caacdd15d9d56180c1e078a05cc065364d51ff
huangyuantong <m13773882201@163.com>
2020-01-12 15:23:47 +0800

    condition_control

---
+++ base/condition_control/if.py
@@ -0,0 +1,19 @@
+if True:
+    print("true")
+else:
+    print("false")
+
+'''
+if condition1:
+    print 1
+elif condition2:
+    print 2
+else:
+    print 3
+'''
+
+x = 5
+y = 8
+print(x == y)
+print(x != y)
+
---
+++ base/condition_control/while.py
@@ -0,0 +1,26 @@
+n = 100
+sum = 0
+counter = 1
+while counter <= 100:
+    sum = sum + counter
+    counter = counter + 1
+print("1 dao %d zhijian de he wei : %d" % (n , sum))
+
+
+var = 1
+while var == 1:
+    num = int(input("输入一个数字:"))
+  i  print("你输入的数字是: ",num) #使用 CTRL+C 来退出当前的无限循环
+
+
+count = 0
+while count < 5:
+    print(count,"< 5")
+    count += 1
+else:
+    print(count,">= 5")
+
+
+flag = 1
+while flag : print("welcome to mynote")
+print("goodbye!")
